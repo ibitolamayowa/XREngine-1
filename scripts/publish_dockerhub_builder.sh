d04de3905bcfd24065fc5397f66deeb0b4b49ed7
Mayowa Ibitola <mrdavidtola@gmail.com>
2022-03-16 17:31:51 +0100

    Fixed unary operator expected issue

--- scripts/publish_dockerhub_builder.sh
+++ scripts/publish_dockerhub_builder.sh
@@ -6,7 +6,7 @@ TAG=$1
 LABEL=$2
 
 
-if [ $PUBLISH_DOCKERHUB == 'true' ]
+if [[ $PUBLISH_DOCKERHUB == 'true' ]]
 then
   echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker tag ${LABEL} ${LABEL}-builder:${TAG}
